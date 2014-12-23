1abd6867e60ebc165e0242ba509405143b87bd04
rdking <jisark5@imac03.jbhunt.com>
2014-12-23 16:47:10 -0600

    A couple of eventful bugs...
    Fixed a bug that prevented listeners from being added properly. This
    prevented those listeners from being fired.
    Fixed a bug where 1-time listeners were not being cleaned up.

--- EventHandler.js
+++ EventHandler.js
@@ -7,48 +7,52 @@ var Final = Class.Final;
 var Property = Class.Property;
 
 var EventHandler = new Class("EventHandler", {
-	listeners: Private({}),
-	
-	addEventListener: Public(function addEventListener(evnt, fn) {
-		if (fn instanceof Function) {
-			if (!this.listeners.hasOwnProperty(evnt))
-				this.listeners[evnt] = { once: false, handlers:[] };
-				
-			this.listeners[evnt].handlers.push(fn);
-		}
-		else
-			throw TypeError("Only functions can be event handlers!");		
-	}),
-	addEventListenerOnce: Public(function addEventListenerOnce(evnt, fn) {
-		if (fn instanceof Function) {
-			if (!this.listeners.hasOwnProperty(evnt))
-				this.listeners[evnt] = [{ once: true, handler:fn }];
+    listeners: Private({}),
+
+    addEventListener: Public(function addEventListener(evnt, fn) {
+        if (fn instanceof Function) {
+            if (!this.listeners.hasOwnProperty(evnt))
+                this.listeners[evnt] = [{ once: false, handler:fn }];
+            else
+                this.listeners[evnt].push({ once: false, handler: fn});
+        }
+        else
+            throw TypeError("Only functions can be event handlers!");
+    }),
+    addEventListenerOnce: Public(function addEventListenerOnce(evnt, fn) {
+        if (fn instanceof Function) {
+            if (!this.listeners.hasOwnProperty(evnt))
+                this.listeners[evnt] = [{ once: true, handler:fn }];
             else
                 this.listeners[evnt].push({ once: true, handler: fn});
-		}		
-		else
-			throw TypeError("Only functions can be event handlers!");		
-	}),
-	removeEventListener: Public(function removeEventListener(evnt, fn) {
-		if (this.listeners.hasOwnProperty(evnt)) {
+        }
+        else
+            throw TypeError("Only functions can be event handlers!");
+    }),
+    removeEventListener: Public(function removeEventListener(evnt, fn) {
+        if (this.listeners.hasOwnProperty(evnt)) {
             var handlers = this.listeners[evnt];
-			var index = -1;
-            
+            var index = -1;
+
             while ((++index < handlers.length) && (handlers[index].handler !== fn));
-			
-			if (index  < handlers.length)
-				this.listeners[evnt].splice(index, 1);
-		}
-	}),
-	fireEvent: Public(function fireEvent(evnt, params) {
-		if (this.listeners.hasOwnProperty(evnt)) {
-			var handlers = this.listeners[evnt];
-			
+
+            if (index  < handlers.length)
+                this.listeners[evnt].splice(index, 1);
+        }
+    }),
+    fireEvent: Public(function fireEvent(evnt, params) {
+        if (this.listeners.hasOwnProperty(evnt)) {
+            var handlers = this.listeners[evnt];
+
             console.log("Processing event: " + evnt);
-			for (var i=0; i<handlers.length; ++i)
-				handlers[i].handler(params);
-		}
-	})
+            for (var i=0; i<handlers.length; ++i) {
+                handlers[i].handler(params);
+
+                if (handlers[i].once)
+                    handlers.splice(i--, 1);
+            }
+        }
+    })
 });
 
 module.exports = EventHandler;

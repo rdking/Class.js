b27ace76ed734dd249b6213b5e063c40589683b7
rdking <kingmph@gmail.com>
2015-06-27 01:40:34 -0500

    Removed reference to Appcelerator's TiSDK.

--- WeakMap.js
+++ WeakMap.js
@@ -17,13 +17,20 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-var WeakMapShim = function() {
+var WeakMapShim = function createWeakMapShim() {
 	//Unique key bank.
 	var keys = [];
 	
-	var guid = function() {
-		return Ti.Platform.createUUID();
-	};
+	var guid = function guid() {
+        function s4() {
+            return Math.floor((1 + Math.random()) * 0x10000)
+                .toString(16)
+                .substring(1);
+        }
+
+        return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+               s4() + '-' + s4() + s4() + s4();
+    };
 	
 	//Constructor
 	var $ = function WeakMap(preload) {		
